d540a2a1490e6f272e574cb90cb0babc989adaac
magkopian <m.agkopian@gmail.com>
2013-10-16 20:51:55 +0300

    Javascript coding standards fixies

--- admin/template/header.php
+++ admin/template/header.php
@@ -23,17 +23,15 @@
 
 		    	// Correct the user while typing the filename
 		    	$('input[name="page_name"]').keydown(function(e) {
-		    		var keyVal = (e.charCode ? e.charCode : ((e.keyCode) ? e.keyCode : e.which));
+		    		var keyVal = ( e.charCode ? e.charCode : ( e.keyCode ? e.keyCode : e.which ) );
 					
-					if ( (keyVal == 0x41 && e.ctrlKey === true) || (keyVal == 0x63 && e.ctrlKey === true) || // Allow: Ctrl+A, or Ctrl+C
-						(keyVal == 0x56 && e.ctrlKey === true) || (keyVal == 0x58 && e.ctrlKey === true ) ) { // or Ctrl+V, or Ctrl+X
+					if ( ( keyVal == 0x41 && e.ctrlKey === true ) || ( keyVal == 0x63 && e.ctrlKey === true ) || // Allow: Ctrl+A, or Ctrl+C
+						( keyVal == 0x56 && e.ctrlKey === true ) || ( keyVal == 0x58 && e.ctrlKey === true ) ) { // or Ctrl+V, or Ctrl+X
 						return;
-					}
-					else if (keyVal == 0x20) { // If user added a space, replace it with underscore
+					} else if ( keyVal == 0x20 ) { // If user added a space, replace it with underscore
 		    			$('input[name="page_name"]').val($(this).val() + '-');
 						return false;
-		    		}
-					else if (keyVal >= 0x41 && keyVal <= 0x5A) { // Convert uppercase letters to lowercase
+		    		} else if ( keyVal >= 0x41 && keyVal <= 0x5A ) { // Convert uppercase letters to lowercase
 						$('input[name="page_name"]').val($(this).val() + String.fromCharCode(keyVal).toLowerCase());
 						return false;
 					}
@@ -45,16 +43,15 @@
 			// Check Description Length
 			$().ready(function(){
 				$('input[name="page_description"]').keypress(function(e) {
-					var len = $(this).val().length;
-					var maxlen = 160;
-					var keyVal = (e.charCode ? e.charCode : ((e.keyCode) ? e.keyCode : e.which));
+					var len = $(this).val().length,
+						maxlen = 160,
+						keyVal = ( e.charCode ? e.charCode : ( e.keyCode ? e.keyCode : e.which ) );
 					
 					if ( keyVal == 8 || keyVal == 9 || keyVal == 13 || //Allow: Backspace, Tab, Enter
-							( keyVal == 0x41 && e.ctrlKey === true) || (keyVal == 0x63 && e.ctrlKey === true) || // Allow: Ctrl+A, or Ctrl+C
-								( keyVal == 0x56 && e.ctrlKey === true) || (keyVal == 0x58 && e.ctrlKey === true ) ) { // or Ctrl+V, or Ctrl+X
+							( keyVal == 0x41 && e.ctrlKey === true ) || ( keyVal == 0x63 && e.ctrlKey === true ) || // Allow: Ctrl+A, or Ctrl+C
+								( keyVal == 0x56 && e.ctrlKey === true ) || ( keyVal == 0x58 && e.ctrlKey === true ) ) { // or Ctrl+V, or Ctrl+X
 						return;
-					}
-					else if ( len > maxlen - 1 ) {
+					} else if ( len > maxlen - 1 ) {
 						return false;
 					}
 				});
