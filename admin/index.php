e811905a5a0ede71d27401e55c87b53f729a1168
themattsparks <themattsparks@gmail.com>
2013-10-16 19:57:31 -0500

    For some reason on certain setups  appending  causign the routing to go wacky. This is a fix for that.

--- admin/index.php
+++ admin/index.php
@@ -40,7 +40,8 @@
  * After that, figure out where to send them.
  */
 	if ( isset( $_GET['page'] ) ) {
-		$requested_page = $_GET['page'];
+		
+		$requested_page = str_replace('.php', '', $_GET['page']);
 
 		// Pages that use the "login_header"
 		switch( $requested_page ) {
